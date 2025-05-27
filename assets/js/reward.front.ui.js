d524c29a6edb105f73d355cc9b106ccab12a79e4
dayeon-da <o0odyyyy@naver.com>
2025-05-27 17:51:07 +0900

    - form 가이드 작업중

--- assets/js/reward.front.ui.js
+++ assets/js/reward.front.ui.js
@@ -90,53 +90,55 @@ rewardPub.front = rewardPub.front || (function () {
   * pram :
   * desc : input 상태 변경(jQuery)
   */
-function setInputStatus(parentSelector) {
+function setInputStatus() {
   let selector = '.ui-input';
-  if (parentSelector) {
-    selector = `${parentSelector} ${selector}`;
-  }
 
   $(selector).each(function () {
     const inputEl = $(this);
     const inputBox = inputEl.closest('.input-box, .input-search-box');
     const btnDelete = inputBox.find('.btn-input-del');
 
+    // 상태 업데이트 함수
     function updateStatus() {
       const hasValue = inputEl.val().length > 0;
+      const isFocused = inputEl.is(':focus');
+      const isDisabled = inputEl.is(':disabled') || inputEl.prop('readonly');
 
-      if (hasValue) {
-        inputBox.addClass('has-value');
+      inputBox.toggleClass('has-value', hasValue);
+
+      if (hasValue && isFocused && !isDisabled) {
+        btnDelete.show();
       } else {
-        inputBox.removeClass('has-value');
+        btnDelete.hide();
       }
     }
 
-    // 입력 및 포커스 이벤트
-    inputEl.off('input focusout').on('input focusout', function () {
+    // focusin: 포커스 시 상태 업데이트
+    inputEl.on('focusin', function () {
       updateStatus();
+    });
 
-      // 포커스 나가면 버튼 숨김
+    // input: 입력 중에도 버튼 보이도록 상태 업데이트
+    inputEl.on('input', function () {
+      updateStatus();
+    });
+
+    // focusout: 다음 루프에서 포커스 없으면 버튼 숨김 (setTimeout)
+    inputEl.on('focusout', function () {
       setTimeout(() => {
         if (!inputEl.is(':focus')) {
           btnDelete.hide();
         }
-      }, 150);
+      }, 0);
     });
 
-    inputEl.on('focusin', function () {
-      // 포커스 되면 버튼 다시 보임
-      if (inputEl.val().length > 0 && !inputEl.is(':disabled') && !inputEl.prop('readonly')) {
-        btnDelete.show();
-      }
-    });
-
-    // 삭제 버튼 클릭 시 값 삭제
-    btnDelete.off('mousedown').on('mousedown', function (e) {
+    // 삭제 버튼 클릭 시: 값 삭제, input 이벤트 발생, 포커스 유지
+    btnDelete.on('mousedown', function (e) {
       e.preventDefault();
       inputEl.val('').trigger('input').focus();
     });
 
-    // 초기 상태
+    // 초기 상태 설정
     updateStatus();
   });
 }
