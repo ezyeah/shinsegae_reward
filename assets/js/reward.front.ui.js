f429fcc05f8f108e965555597ad15d14d52501a8
dayeon-da <o0odyyyy@naver.com>
2025-05-27 15:15:03 +0900

    - form 가이드 작업중

--- assets/js/reward.front.ui.js
+++ assets/js/reward.front.ui.js
@@ -79,3 +79,66 @@ rewardPub.front = rewardPub.front || (function () {
 
   return _front;
 })();
+
+
+  /*
+  * date : 20259999
+  * last : 20259999
+  * name : setInputStatus()
+  * pram :
+  * desc : input 상태 변경(jQuery)
+  */
+function setInputStatus(parentSelector) {
+  let selector = '.ui-input';
+  if (parentSelector) {
+    selector = `${parentSelector} ${selector}`;
+  }
+
+  $(selector).each(function () {
+    const inputEl = $(this);
+    const inputBox = inputEl.closest('.input-box, .input-search-box');
+    const btnDelete = inputBox.find('.btn-input-del');
+
+    function updateStatus() {
+      const hasValue = inputEl.val().length > 0;
+
+      if (hasValue) {
+        inputBox.addClass('has-value');
+      } else {
+        inputBox.removeClass('has-value');
+      }
+    }
+
+    // 입력 및 포커스 이벤트
+    inputEl.off('input focusout').on('input focusout', function () {
+      updateStatus();
+
+      // 포커스 나가면 버튼 숨김
+      setTimeout(() => {
+        if (!inputEl.is(':focus')) {
+          btnDelete.hide();
+        }
+      }, 150);
+    });
+
+    inputEl.on('focusin', function () {
+      // 포커스 되면 버튼 다시 보임
+      if (inputEl.val().length > 0 && !inputEl.is(':disabled') && !inputEl.prop('readonly')) {
+        btnDelete.show();
+      }
+    });
+
+    // 삭제 버튼 클릭 시 값 삭제
+    btnDelete.off('mousedown').on('mousedown', function (e) {
+      e.preventDefault();
+      inputEl.val('').trigger('input').focus();
+    });
+
+    // 초기 상태
+    updateStatus();
+  });
+}
+
+$(document).ready(function () {
+  setInputStatus();
+});
